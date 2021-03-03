a75d46645e397343daed1a9e846e1c8214211334
ToxPuro <puro.touko2@gmail.com>
2021-03-03 19:50:39 +0200

    ÄÄH

--- index.js
+++ index.js
@@ -94,6 +94,6 @@ app.post('/api/persons', (req,res) =>{
 
 })
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`)
 })
